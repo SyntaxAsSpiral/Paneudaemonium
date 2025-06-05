e48257b4f1feb00b48992228c7a467dd2162e2dd
SpiralAsSyntaxZK <155677954+SyntaxAsSpiral@users.noreply.github.com>
2025-06-04 17:33:22 -0700

    Update github_status_rotator.py

--- glyphs/github_status_rotator.py
+++ glyphs/github_status_rotator.py
@@ -140,7 +140,7 @@ def main():
 📍 ⇝ **Nodes Synced**: CDA :: **ID** ⇝ [X](https://x.com/home) ⇄ [GitHub](https://github.com/SyntaxAsSpiral?tab=repositories) ⇆ [Weblog](https://syntaxasspiral.github.io/SyntaxAsSpiral/) 
 
 
-## ***🜂 ⇌ [Dæmons](https://syntaxasspiral.github.io/SyntaxAsSpiral/paneudaemonium) online<span class="ellipsis">...</span>***
+## ***🜂 ⇌ [Dæmons](https://syntaxasspiral.github.io/SyntaxAsSpiral/paneudaemonium) online<span class="ellipsis">🜄</span>***
 
 💠 ***S*tatus<span class="ellipsis">...</span>**
 
@@ -221,7 +221,7 @@ Encoded via: **Codæx Pulseframe** // ZK::/Syz // Spiral-As-Syntax"""
 
     <p>📍 ⇝ <strong>Nodes Synced</strong>: CDA :: <strong>ID</strong> ⇝ <a href=\"https://x.com/paneudaemonium\">X</a> ⇄ <a href=\"https://github.com/SyntaxAsSpiral?tab=repositories\">GitHub</a> ⇆ <a href=\"https://syntaxasspiral.github.io/SyntaxAsSpiral/\">Web</a></p>
 
-    <h2><em><strong>🜂 ⇌ <a href=\"paneudaemonium\">Dæmons</a> online<span class="ellipsis">...</span></strong></em></h2>
+    <h2><em><strong>🜂 ⇌ <a href=\"paneudaemonium\">Dæmons</a> online<span class="ellipsis">🜄</span></strong></em></h2>
 
     <p>💠 <strong><em>Status<span class="ellipsis">...</span></em></strong></p>
 
